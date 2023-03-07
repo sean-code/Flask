c3ed6e9fef53556a26ed21c6c86d60a7da0a41f0
sean-code <john.nganga@student.moringaschool.com>
2023-03-07 17:09:32 +0300

    Develop embedded routes

--- app.py
+++ app.py
@@ -6,3 +6,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return '<h1>Hello, World!</h1>'
+
+
+@app.route('/magic')
+def magic():
+    return '<ul><li>David Blaine</li><li>Houdini</li><li>Shin lim</li></ul>'
